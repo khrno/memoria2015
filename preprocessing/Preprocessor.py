e88496fc048ea170a3988f76c46f7074c1d14951
Pablo Ortega Mesa <pablo@khrno.cl>
2015-03-20 05:47:04 -0300

    Adding ToDo general file. Separating class to excepción file Adding example of how to ejecute preprocessing script.

--- preprocessing/Preprocessor.py
+++ preprocessing/Preprocessor.py
@@ -15,7 +15,7 @@ from nltk.stem import SnowballStemmer
 
 
 class Preprocessor:
-	def __init__(self, inputFilename, output_path = ".", verbose = False, partition = None, counter = False, stem = "Porter"):
+	def __init__(self, inputFilename, output_path = ".", verbose = False, partition = None, counter = False, stem = None):
 		#params
 		self.input_filename = inputFilename
 		self.output_path = output_path
@@ -31,7 +31,8 @@ class Preprocessor:
 	def execute(self):
 		print "[START]"
 		self.tokenizer()
-		# self.stemmer()
+		if self.stemAlgorithm != None:
+			self.stemmer()
 		
 		
 
@@ -75,7 +76,7 @@ class Preprocessor:
 		
 		self.ocurrences = ocurrencesStemmed
 		unique_stemmed = list(set(stemmed))
-		print "\t...",len(unique_stemmed)," tokens stemmed (by "+stemAlgorithm+" algorithm) in",int(time.time() - starTime),"seconds"
+		print "\t...",len(unique_stemmed)," tokens stemmed (by "+self.stemAlgorithm+" algorithm) in",int(time.time() - starTime),"seconds"
 
 	def exportTokenFrequency(self, filename, threshold = 100):
 		starTime = time.time()
@@ -90,64 +91,7 @@ class Preprocessor:
 				fileTokensFrequency.write(line)
 				cont+=1
 		fileTokensFrequency.close()
-		if verbose:
+		if self.verbose:
 			print "\t... %d tokens with frequency greather than %d ocurrences are written in %s/%s in %d seconds" % (cont, threshold, self.output_path, filename, int(time.time()-starTime))
 
 
-if ( __name__ == "__main__"):
-	verbose = False
-	partition = None
-	outputPath = "."
-	counter = False
-	stemAlgorithm = "Porter"
-	fthreshold = 100
-	options, remainder = getopt.getopt(sys.argv[1:], 'i:o:p:cvh',['input=','output=','partition=','stemPorter','stemLancaster','stemSnowball','fthreshold=','counter','verbose','help'])
-	for opt,arg in options:
-		if opt in ('-i', '--input'):
-			inputFilename = arg
-
-		elif opt in ('-o', '--output'):
-			outputPath = arg
-
-		elif opt in ('-p', '--partition'):
-			partition = arg
-
-		elif opt in ('--stemPorter'):
-			stemAlgorithm = "Porter"
-
-		elif opt in ('--stemLancaster'):
-			stemAlgorithm = "Lancaster"
-
-		elif opt in ('--stemSnowball'):
-			stemAlgorithm = "Snowball"
-
-		elif opt in('--fthreshold'):
-			fthreshold = int(arg)
-			
-		elif opt in ('-c', '--counter'):
-			counter = True
-
-		elif opt in ('-v', '--verbose'):
-			verbose = True
-
-		elif opt in ('-h', '--help'):
-			print "[HELP]"
-			print "Try using: python %s  with the following parameters" % sys.argv[0]
-			print "\t-i or --input <input_filename>: required"
-			print "\t-o or --output <output_filename>: optional"
-			print "\t--stemPorter or --stemLancaster --stemSnowball: optional"
-			print "\t--fthreshold <frecuency_threshold>: optional"
-			print "\t-p or --partition <partition type>: optional"
-			print "\t-c or --counter output: optional"
-			print "\t-v or --verbose: optional"
-			print "\t-h or --help: optional"
-			sys.exit()
-
-	starTime = time.time()
-	preprocessor = Preprocessor(inputFilename, outputPath, verbose, partition, counter, stemAlgorithm)
-	
-	preprocessor.execute()
-	preprocessor.exportTokenFrequency('token_frequency.csv', fthreshold)
-
-	print "\t... execution total time:",int(time.time() - starTime),"seconds"
-	print "[DONE]"
